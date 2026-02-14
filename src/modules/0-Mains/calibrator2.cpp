45327bc49d3b46b92e6f5e74a0452a90871db1f1
LuciferLied <Benjamin_porsmose@hotmail.com>
2026-02-15 00:14:20 +0100

    ..

--- src/modules/0-Mains/calibrator2.cpp
+++ src/modules/0-Mains/calibrator2.cpp
@@ -4,37 +4,8 @@ void calibrator2(unsigned long now) {
   diffADCVals = 0;
   currentPH = phValues[pHValuesIndex];
   analDicoArrIndex = 0;
-
-  int usedSpace = 0;
-  getADCSamples(calibValueArr, calibOccurArr, 100, usedSpace);
+  usedSize = 0;
+  getADCSamples(calibValueArr, calibOccurArr, 100, usedSize);
 
   sortData(calibValueArr, calibOccurArr, 100);
-  trimMeanArray[trimMeanArrayIndex] = trimmedMean(analDiCoArr, analDiCoArrSize, trimPercADCCalib);
-  trimMeanArrayIndex++;
-  if (trimMeanArrayIndex == trimMeanArrSize || enoughSamples == true) {
-    enoughSamples = true;
-    stdDevOfTrim = StdDev(trimMeanArray, trimMeanArrSize);
-    trimOfTrim = trimmedMean(trimMeanArray, trimMeanArrSize, trimTrimPercADCCalib);
-    mediOfTrim = getMedianValue(trimMeanArray, trimMeanArrSize);
-    if (trimMeanArrayIndex == trimMeanArrSize) {
-      trimMeanArrayIndex = 0;
-    }
-    int holder[diffADCVals];
-    float holder2[diffADCVals];
-    clusterer(clusteredSamplesArray, diffADCVals, mediOfTrim, holder);
-    for (int i = 0; i < diffADCVals; i++) {
-      if (holder[i] != 0) {
-        medianOfTrimCluster = clusteredSamplesArray[i];
-      }
-      holder[i] = 0;
-    }
-
-    calibClusterer(clusteredSamplesArray, diffADCVals, trimOfTrim, holder);
-    for (int i = 0; i < diffADCVals; i++) {
-      if (holder2[i] != 0) {
-        trimOfTrimCluster = clusteredSamplesArray[i];
-      }
-      holder[i] = 0;
-    }
-  }
 }
--- src/modules/6-calibration/getADCSamples.cpp
+++ src/modules/6-calibration/getADCSamples.cpp
@@ -1,7 +1,7 @@
 #include <config.h>
 
 void getADCSamples(int values[], int occurrences[], int size, int &usedSpace) {
-  int sampleBatches = 6;
+  int sampleBatches = 36;
   int sammpleIndex = 0;
   for (int i = 0; i < size; i++) {
     values[i] = 0;
