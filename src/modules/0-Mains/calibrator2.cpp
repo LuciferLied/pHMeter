e0d8642ce3423e7f6abec40086d5d27379c475f2
LuciferLied <Benjamin_porsmose@hotmail.com>
2026-02-15 09:50:30 +0100

    ..

--- src/modules/0-Mains/calibrator2.cpp
+++ src/modules/0-Mains/calibrator2.cpp
@@ -8,18 +8,7 @@ void calibrator2(unsigned long now) {
   syncShiftArrays(calibValueArr, calibOccurArr, 100);
   CalibOccMedi = calculateMedian(calibValueArr, calibOccurArr, 100);
   CalibOccTrim = calculateTrimmedMean(calibValueArr, calibOccurArr, 100, 0.25);
-  // largeWindow = highestOcc*0.20;
-  // medWindow = highestOcc*0.36;
-  // smallWindow = highestOcc*0.60;
-  largeWindow = 1;
-  medWindow = 1;
-  smallWindow = 1; 
-  // for (int i = 0; i < 100; i++)
-  // {
-  //   if(calibOccurArr[i]> highestOcc*0.60){
-  //   Serial.print(calibValueArr[i]);
-  //   Serial.print(" - ");
-  //   Serial.println(calibOccurArr[i]);
-  //   }
-  // }
+  largeWindow = highestOcc*0.20;
+  medWindow = highestOcc*0.36;
+  smallWindow = highestOcc*0.60;
 }
