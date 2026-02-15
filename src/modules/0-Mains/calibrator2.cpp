348f62ddaaee189135060485eb418d2ee68dfa51
LuciferLied <Benjamin_porsmose@hotmail.com>
2026-02-15 11:06:24 +0100

    Update calibrator2.cpp

--- src/modules/0-Mains/calibrator2.cpp
+++ src/modules/0-Mains/calibrator2.cpp
@@ -11,6 +11,7 @@ void calibrator2(unsigned long now) {
   CalibOccMedi = calculateMedian(calibValueArr, calibOccurArr, 100);
   CalibOccTrim = calculateTrimmedMean(calibValueArr, calibOccurArr, 100, 0.25);
   setWindows();
+  Serial.println(highestOcc);
   Serial.println(largeWindow);
   Serial.println(mediumWindow);
   Serial.println(smallWindow);
