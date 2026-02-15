ff8d4bb1a6064df67e62d37b8b3fd82acd9609aa
LuciferLied <Benjamin_porsmose@hotmail.com>
2026-02-15 01:48:12 +0100

    ..

--- src/modules/0-Mains/calibrator2.cpp
+++ src/modules/0-Mains/calibrator2.cpp
@@ -11,6 +11,6 @@ void calibrator2(unsigned long now) {
   CalibOccMedi = calculateMedian(calibValueArr, calibOccurArr, 100);
   CalibOccTrim = calculateTrimmedMean(calibValueArr, calibOccurArr, 100, 0.25);
   largeWindow = 100;
-  medWindow = highestOcc*0.5;
-  smallWindow =  - highestOcc*0.75;
+  medWindow = highestOcc*0.36;
+  smallWindow = highestOcc*0.75;
 }
